c8b4658e0be4f924df7650091ee88f735130bd47
Felix K <TankiOnlineIstGeil@hotmail.de>
2019-11-09 13:14:57 +0100

    added vs project

---
+++ zum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
