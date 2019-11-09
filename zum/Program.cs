1e5f9b97dfd7b83900e06545d20d18b8545a748c
Felix K <julius.tzunami@yahoo.de>
2019-11-09 15:26:22 +0100

    async Main, aync thread fix and channel setup

--- zum/Program.cs
+++ zum/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace zum
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Database.ConnectionString = "Server=localhost;Database=dbname;Uid=root;Pwd=password;";
+            await Database.connection.PingAsync();
+            Global.SetupChannels();
             new Server(5001);
+            await Task.Delay(-1);
         }
     }
 }
